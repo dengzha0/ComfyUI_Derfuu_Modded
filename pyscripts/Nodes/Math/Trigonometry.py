53e4b9c8952460d3a6ea5a1d402961cf4a06174b
Dengzha0 <dengz@outlook.com>
2024-12-26 10:36:32 +0800

    Update Trigonometry.py
    change CosNode to a new node

--- pyscripts/Nodes/Math/Trigonometry.py
+++ pyscripts/Nodes/Math/Trigonometry.py
@@ -3,7 +3,7 @@ import math
 from ...components.fields import Field
 from ...components.tree import TREE_TRIGONOMETRY
 
-
+# changed to new node Greater
 class SinNode:
     def __init__(self):
         pass
@@ -29,7 +29,7 @@ class SinNode:
             greater = value_b
         return (greater,)
 
-
+# changed to new node "FilterSize"
 class CosNode:
     def __init__(self):
         pass
@@ -38,24 +38,48 @@ class CosNode:
     def INPUT_TYPES(self):
         return {
             "required": {
-                "value": Field.float(),
-                "type_": Field.combo(["RAD", "DEG"],),
-                "arcCos": Field.combo([False, True],)
+                "width": Field.int(),
+                "height": Field.int(),
+                "small_size": Field.int(),
+                "big_size": Field.int()
             }
         }
 
-    RETURN_TYPES = ("FLOAT",)
+    RETURN_TYPES = ("INT","INT",)
+    RETURN_NAMES = ("WIDTH", "HEIGHT")
     FUNCTION = "get_value"
     CATEGORY = TREE_TRIGONOMETRY
 
-    def get_value(self, value, type_="RAD", arcCos=False):
-        if type_ == "DEG":
-            value = math.radians(value)
-        if arcCos == True:
-            value = math.acos(value)
+    def get_value(self, width, height, small_size, big_size):
+        larger_side = ""
+        if width > height:
+            larger_side = "width"
         else:
-            value = math.cos(value)
-        return (value,)
+            larger_side = "height"
+
+        new_width = 0
+        new_height = 0
+        ratio = 0
+        if larger_side == "width":
+            ratio = height / width
+            if width < small_size:
+                new_width = small_size
+                new_height = int(new_width * ratio / 8) * 8
+            else:
+                new_width = big_size
+                new_height = int(new_width * ratio / 8) * 8
+        elif larger_side == "height":
+            ratio = width / height
+            if height < small_size:
+                new_height = small_size
+                new_width = int(new_height * ratio / 8) * 8
+            else:
+                new_height = big_size
+                new_width = int(new_height * ratio / 8) * 8
+        else:
+            new_width = width
+            new_height = height
+        return (new_width,new_height,)
 
 
 class tgNode:
